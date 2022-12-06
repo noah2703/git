58a8c4e32968e18e08c3919977608c8735b2cbaf
root <root@ip-172-31-81-54.ec2.internal>
2022-12-06 20:03:29 +0000

    V1.2 ajout création superutilisateur

--- script.sh
+++ script.sh
@@ -7,9 +7,20 @@ cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
 
 cp ./config__files/ssh/Banner /etc/Banner
 
+#####  Création d'un user   ######
 
-# Redemarrer le service sshd 
+echo -e "\nVeuillez ecrire votre nom d'utilisateur :\n"
+read USERNAME
 
-systemctl restart sshd 
+useradd -m -d /home/$USERNAME -s /bin/bash $USERNAME
+echo -e "\nUtilisateru crée\n"
 
-echo "succes"
+echo -e "\nVeuillez saisir un mot de passe :\n"
+passwd $USERNAME
+
+usermod -aG sudo $USERNAME
+echo -e "l'Utilisateur est devenue root"
+
+# Redemarrer le service sshd
+
+systemctl restart sshd
