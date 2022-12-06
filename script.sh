31ba6d53b5be78d0c28d21b229a8ee7c38341c02
noah <noah.pradel02@gmail.com>
2022-12-06 22:03:06 +0100

    v1.3 ajout autentification par clé ssh

--- script.sh
+++ script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
+#####   configuration SSH    ######
+
+
 # Copier le fichier de configuration sur le serveur
-
 cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
-
 cp ./config__files/ssh/Banner /etc/Banner
 
+
 #####  Création d'un user   ######
+
 
 echo -e "\nVeuillez ecrire votre nom d'utilisateur :\n"
 read USERNAME
@@ -18,8 +21,19 @@ echo -e "\nUtilisateru crée\n"
 echo -e "\nVeuillez saisir un mot de passe :\n"
 passwd $USERNAME
 
+# ajout au grroupe sudo 
 usermod -aG sudo $USERNAME
 echo -e "l'Utilisateur est devenue root"
+
+
+#####   Authentification par clé    ######
+
+
+echo -e "\nVeuillez entrer votre clé :\n"
+read KEY
+
+echo $KEY >> ~/.ssh/authorized_keys
+echo -e "\nVotre clé a été ajoutée !!\n"
 
 # Redemarrer le service sshd
 
